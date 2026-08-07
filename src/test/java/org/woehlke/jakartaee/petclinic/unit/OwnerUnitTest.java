5b42bb7914cfd28f0b3970b414a01a445819c1c8
thomaswoehlkebochum <thomas.woehlke.bochum@gmail.com>
2026-08-07 21:01:40 +0200

    working on #5

--- src/test/java/org/woehlke/jakartaee/petclinic/unit/OwnerUnitTest.java
+++ src/test/java/org/woehlke/jakartaee/petclinic/unit/OwnerUnitTest.java
@@ -5,11 +5,8 @@ import lombok.extern.java.Log;
 import org.junit.jupiter.api.*;
 import org.woehlke.jakartaee.petclinic.deployments.UnitTestData;
 import org.woehlke.jakartaee.petclinic.owner.Owner;
-import org.woehlke.jakartaee.petclinic.pet.Pet;
 
-import java.time.Instant;
 import java.util.*;
-import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -140,84 +137,4 @@ public class OwnerUnitTest extends UnitTestData {
             firstEntity = secondEntity;
         }
     }
-
-    /*
-    @Test
-    @Order(8)
-    public void testGetPetst01(){
-        log.info("testGetPetst01");
-        int expectedSize  = 0;
-        Owner entity = new Owner();
-        int size = entity.getPetsAsList().size();
-        assertThat(expectedSize == size);
-    }
-
-    @Test
-    @Order(9)
-    public void testGetPetst02(){
-        log.info("testGetPetst02");
-        Owner entity = ownerList.get(2);
-        String petName = UnitTestData.petNames[2];
-        int size = 0;
-        //int expectedSize  = entity.getPetsAsList().size() + 1;
-        int expectedSize  = size + 1;
-        assertThat("Nelly".compareTo(petName)==0);
-        Pet pet = new Pet();
-        pet.setName(petName);
-        pet.setBirthDate((Date)Date.from(Instant.now()));
-        pet.setUuid(UUID.randomUUID());
-        entity.addPet(pet);
-        int size = entity.getPetsAsList().size();
-        assertThat(expectedSize == size);
-    }
-
-    @Test
-    @Order(10)
-    public void testGetPetst03(){
-        log.info("testGetPetst03");
-        Owner entity = ownerList.get(2);
-        String petName = petNames[2];
-        int expectedSize  = entity.getPetsAsList().size() + 1;
-        assertThat("Nelly".compareTo(petName)==0);
-        Pet pet = new Pet();
-        pet.setBirthDate((Date)Date.from(Instant.now()));
-        pet.setName(petName);
-        pet.setUuid(UUID.randomUUID());
-        pet.setOwner(entity);
-        //entity.addPet(pet);
-        pet = entity.getPetsAsList().iterator().next();
-        assertThat(petName ==  pet.getName());
-    }
-
-    @Test
-    @Order(11)
-    public void testGetPetst04() {
-        log.info("testGetPetst04");
-        Owner entity = ownerList.get(3);
-        int expectedSize = petSet.size();
-        entity.setPets(petSet);
-        int size = entity.getPetsAsList().size();
-        assertThat(expectedSize == size);
-    }
-
-    @Test
-    @Order(12)
-    public void testGetPetst05() {
-        log.info("testGetPetst05");
-        Owner entity = ownerList.get(3);
-        int expectedSize = petSet.size();
-        entity.setPets(petSet);
-        int size = entity.getPetsAsList().size();
-        assertThat(expectedSize == size);
-        assertThat(petList.size() > 2);
-        Iterator<Pet> i =  petList.iterator();
-        Pet first = i.next();
-        while(i.hasNext()){
-            Pet second = i.next();
-            int compared = second.compareTo(first);
-            assertThat(compared > 0);
-            first = second;
-        }
-    }
-    */
 }
